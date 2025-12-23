2698d270f10f6401e4c5cf82df63bfc2aac78686
Gedu-Dharminder <dsingh2@geduservices.com>
2025-12-23 15:30:45 +0530

    add another heading and aslo chahe the formate

--- components/editors/heading-editor.jsx
+++ components/editors/heading-editor.jsx
@@ -13,6 +13,9 @@ export function HeadingEditor({ editData, setEditData }) {
           <option value="h1">H1</option>
           <option value="h2">H2</option>
           <option value="h3">H3</option>
+          <option value="h4">H4</option>
+          <option value="h5">H5</option>
+          <option value="h6">H6</option>
         </select>
       </div>
       <div>
